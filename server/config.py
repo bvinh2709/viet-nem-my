ce005abae0afd48c007a572e4c80dbe1dad2f2b2
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-06-15 00:36:44 -0500

    ready for deployment

--- server/config.py
+++ server/config.py
@@ -4,20 +4,32 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-
+from flask_bcrypt import Bcrypt
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.permanent_session_lifetime = timedelta(days=30)
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
+
 db = SQLAlchemy(metadata=metadata)
 
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
 api = Api(app)
+
+bcrypt = Bcrypt(app)
 
 CORS(app)
